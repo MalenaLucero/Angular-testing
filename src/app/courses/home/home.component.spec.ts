f5464f4b88b971766b479cfbf973866eae2027a1
Malena Lucero <Malena.Lucero@endava.com>
2022-02-16 15:17:49 -0300

    3-component-testing

---
+++ src/app/courses/home/home.component.spec.ts
@@ -0,0 +1,102 @@
+import {ComponentFixture, fakeAsync, flush, flushMicrotasks, TestBed, waitForAsync} from '@angular/core/testing';
+import {CoursesModule} from '../courses.module';
+import {DebugElement} from '@angular/core';
+
+import {HomeComponent} from './home.component';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {CoursesService} from '../services/courses.service';
+import {HttpClient} from '@angular/common/http';
+import {COURSES} from '../../../../server/db-data';
+import {setupCourses} from '../common/setup-test-data';
+import {By} from '@angular/platform-browser';
+import {of, from} from 'rxjs';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {click} from '../common/test-utils';
+
+
+
+
+describe('HomeComponent', () => {
+
+  let fixture: ComponentFixture<HomeComponent>;
+  let component:HomeComponent;
+  let el: DebugElement;
+  let coursesService: any;
+
+  const beginnerCourses = setupCourses()
+    .filter(course => course.category == 'BEGINNER')
+  const advancedCourses = setupCourses().filter(course => course.category == 'ADVANCED')
+
+  beforeEach(waitForAsync(() => {
+    const coursesServiceSpy = jasmine.createSpyObj('CoursesService', ['findAllCourses'])
+    TestBed.configureTestingModule({
+      //NoopAnimationsModule: there won't be animations
+      imports: [CoursesModule, NoopAnimationsModule],
+      providers: [
+        { provide: CoursesService, useValue: coursesServiceSpy}
+      ]
+    }).compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(HomeComponent)
+        component = fixture.componentInstance
+        el = fixture.debugElement
+        coursesService = TestBed.inject(CoursesService)
+      })
+  }));
+
+  it("should create the component", () => {
+
+    expect(component).toBeTruthy();
+
+  });
+
+
+  it("should display only beginner courses", () => {
+    console.log('it should only display beginner')
+    console.log(beginnerCourses)
+    //the test is synchronous
+    coursesService.findAllCourses.and.returnValue(of(beginnerCourses))
+    fixture.detectChanges()
+    const tabs = el.queryAll(By.css('.mat-tab-label'))
+    expect(tabs.length).toBe(1, 'Unexpected number of tabs')
+  });
+
+
+  it("should display only advanced courses", () => {
+    
+    coursesService.findAllCourses.and.returnValue(of(advancedCourses))
+    fixture.detectChanges()
+    const tabs = el.queryAll(By.css('.mat-tab-label'))
+    expect(tabs.length).toBe(1, 'Unexpected tabs found')
+  
+  });
+
+
+  it("should display both tabs", () => {
+
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()))
+    fixture.detectChanges()
+    const tabs = el.queryAll(By.css('.mat-tab-label'))
+    expect(tabs.length).toBe(2, 'Unexpected tabs found')
+
+  });
+
+
+  it("should display advanced courses when tab clicked", () => {
+
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()))
+    fixture.detectChanges()
+    const tabs = el.queryAll(By.css('.mat-tab-label'))
+    //simulation of click by the user
+    //click utility function
+    click(tabs[1])
+    fixture.detectChanges()
+    const cardTitles = el.queryAll(By.css('.mat-card-title'))
+    expect(cardTitles.length).toBeGreaterThan(0, 'could not find card titles')
+    expect(cardTitles[0].nativeElement.textContent).toContain('Angular Security Course')
+
+  });
+
+});
+
+
--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -10,7 +10,7 @@ describe('CalculatorService', () => {
     let loggerSpy: any
 
     beforeEach(() => {
-        console.log('Calling beforeEach')
+       
         //const logger = new LoggerService() --> actual implementation of service 
         //spyOne(logger, 'log') --> spying on a specific method
 
@@ -35,7 +35,7 @@ describe('CalculatorService', () => {
     
     //test specification
     it('should add two numbers', () => {
-        console.log('add test')
+    
         const result = calculator.add(2, 2)
         expect(result).toBe(4)
         expect(loggerSpy.log).toHaveBeenCalledTimes(1)
@@ -43,7 +43,7 @@ describe('CalculatorService', () => {
 
     //test specification
     it('should substract two numbers', () => {
-        console.log('substract test')
+       
         const result = calculator.subtract(2, 2)
         //second argument: error message
         expect(result).toBe(0, 'unexpected substraction result')
